71914bc4a53e25f1aafff5499ead50e13cca72f9
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-16 05:48:03 +0300

    capitalized U in Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum of list of floats and integers"""
     return sum(mxd_lst)
