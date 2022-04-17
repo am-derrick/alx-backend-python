4b6e85606d85f712744bf87d9313361ca7276841
Derrick Ampire <ampirre90@gmail.com>
2022-04-17 04:55:05 +0000

    async tasks added

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python3
+"""asyncio tasks"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """waits for random delay and returns delay"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delay = [await task for task in asyncio.as_completed(tasks)]
+    return delay
