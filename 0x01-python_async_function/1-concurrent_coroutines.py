8a2760b19464895a518d8ea65049e64af319cc8a
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-17 07:59:14 +0300

    typo fixed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').waitt_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """waits wait_random n times with specified max_delay"""
