79d61ba7600d2af31d8f690d39702f1822350128
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-19 15:44:49 +0300

    pep fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """coroutines collects 10 random numbers using async comprehension over
-    async_generator and returns 10 random numbers"""
+    async_generator and returns 10 random numbers
+    """
     l = [i async for i in async_generator()]
     return l
